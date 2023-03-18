327d6ad3d09533255264f3ef50d5c2235912f177
AthiestCoder <120329747+AthiestCoder@users.noreply.github.com>
2023-03-18 10:57:41 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var firstName = "Victor";
+console.log(firstName);
+var lastName = "Fernandes";
+console.log(lastName);
+var fullName = concat(firstName,lastName);
+console.log(fullName);
+var number1 = 3.14159
+console.log(number1);
+var number2 = 2.71828
+console.log(number2);
+var min = min(number1,number2)
+console.log(min)
+var max = max(number1,number2)
+console.log(max)
